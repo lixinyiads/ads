07479e0fa6e042eac6d78c08ed290e54a35ea791
lixinyiads <lixinyiads@users.noreply.github.com>
2016-01-06 13:55:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<script>
-clickurl1="http://p.ato.mx/placement?v=8&id=10572&type=iframe&size=300x250&type=iframe&b=0&domain=http%3A%2F%2Fwww.autotrader.com&screen=320x568x32&timezone=-240&cookies=1&flash=1&r=[CS::REFERER]";
-             document.writeln("<iframe src="+clickurl1+"></iframe>");
-</script>
+
+<iframe src="http://p.ato.mx/placement?v=8&id=10572&type=iframe&size=300x250&type=iframe&b=0&domain=http%3A%2F%2Fwww.autotrader.com&screen=320x568x32&timezone=-240&cookies=1&flash=1&r=[CS::REFERER]"></iframe>
+            
+
